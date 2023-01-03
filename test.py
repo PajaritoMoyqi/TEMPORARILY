5c1265c3c48ccc6e9df466a41199e770761e6f1a
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-01-03 14:23:51 +0900

    for kdt answer

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-import module
-from module import var, function, Class
-from module import *
+num = int(input('정수만 입력해주세요, 전 당신을 믿습니다: '))
 
-from package import module
-from package.module import var, function, Class
+if num > 0:
+    print('양의 정수를 입력해주셨네요!')
+elif num < 0:
+    print('음의 정수를 입력해주셨군요.')
+else:
+    print('혹시 0을 입력하셨나요?')
