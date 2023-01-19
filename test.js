7d9864a4ad427b44a05ad5a166456a392a5cc13d
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-01-19 09:17:15 +0900

    test.js - for Javascript Essay

--- test.js
+++ test.js
@@ -1,12 +1,1 @@
-const obj = {};
-
-const mySymbol1 = Symbol("test");
-const mySymbol2 = Symbol("test");
-obj[mySymbol1] = "test1";
-obj[mySymbol2] = "test2";
-
-console.log(obj); // { [Symbol(test)]: "test1", [Symbol(test)]: "test2" }
-console.log(obj[mySymbol1]); // "test1"
-console.log(obj[mySymbol2]); // "test2"
-console.log(mySymbol1);
-console.log(mySymbol2);
+console.log(parseInt("42px", 25));
