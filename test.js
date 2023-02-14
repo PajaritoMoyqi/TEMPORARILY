11a561a07c49ef96e2833d92727d07ea448ae26f
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-02-14 18:22:05 +0900

    test.js for Javascript Essay

--- test.js
+++ test.js
@@ -1,1 +1,11 @@
-console.log(parseInt("42px", 25));
+var a = "str";
+var b = "1";
+
+switch (true) {
+  case (a == 1 || b == 1):
+    console.log("true is str");
+    break;
+  default:
+    console.log("true is not str");
+    break;
+}
