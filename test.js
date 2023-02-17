bd83241fe95e93987e8753328dc1234972d9161f
PajaritoMoyqi <pajaritomoyqi@protonmail.ch>
2023-02-17 12:15:04 +0900

    test.js - Javascript Essay

--- test.js
+++ test.js
@@ -1,16 +1,8 @@
-for(let i=0; i<10; i++){
-  console.log(i);
+for (var i=1; i<=5; i++) {
+  (function (){
+    var j = i;
+    setTimeout( function timer() {
+      console.log(j);
+    }, 1000);
+  })();
 }
-console.log(i); // ReferenceError: i is not defined
-
-
-loading = false;
-
-if(!loading){
-  const limit = 5;
-  const result = makeSomething(limit);
-
-  console.log(result);
-}
-
-console.log(limit); // ReferenceError: limit is not defined
