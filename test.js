e9c88ad4480e57eed2ff03fc3803a49975bd52c7
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-02-28 20:44:29 +0900

    test.js for JS Essay check

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
-for (var i=1; i<=5; i++) {
-  (function (){
-    var j = i;
-    setTimeout( function timer() {
-      console.log(j);
-    }, 1000);
-  })();
-}
+parseInt("0x11"); // 17
+parseInt("0x11", 16); // 17
+parseInt("011"); // 11
+parseInt("011", 8); // 9
+
+console.log(parseInt("x11"));
+console.log(parseInt("0x11"));
+console.log(parseInt("0x11", 16));
+console.log(parseInt("011"));
+console.log(parseInt("011", 8));
