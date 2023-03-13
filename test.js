23fe82d21ceefb4d59c303fe02e7d5f487bcfbf2
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-03-14 08:32:16 +0900

    basic slider done - test.htmlk

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
-parseInt("0x11"); // 17
-parseInt("0x11", 16); // 17
-parseInt("011"); // 11
-parseInt("011", 8); // 9
+function foo() {
+  console.log(this.a);
+}
 
-console.log(parseInt("x11"));
-console.log(parseInt("0x11"));
-console.log(parseInt("0x11", 16));
-console.log(parseInt("011"));
-console.log(parseInt("011", 8));
+var obj = {
+  a: 2,
+  foo: foo,
+  baz: function() {
+    console.log(this.a);
+  }
+}
+
+var bar = obj.foo;
+var bar2 = obj.baz;
+var a = "전역";
+
+bar2();
+foo();
+bar();
