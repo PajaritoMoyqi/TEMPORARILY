3c8ff34d52e83cf3f4a5d5ca05f5549bec252d96
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2023-07-28 13:55:16 +0900

    test.c for C Summary essay(yesterday)

--- test.c
+++ test.c
@@ -1,8 +1,27 @@
+#include <stdio.h>
 
+#define NAME "Pajarito"
+#define EMAIL "Pajarito@pajaritoemail.com"
 
 int main( void )
 {
-    int arr[3][2] = { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+    int i = 0xFF;
+    unsigned ui = 0xFFFFFFFF;
 
-    printf( "%d", arr );
+    printf( "i: %d, %x, %X, %#X, %o, %#o\n", i, i, i, i, i, i );
+    printf( "ui: %u, %#x, %d\n", ui, ui, ui );
+    printf( "My name is" NAME "." );
+    printf( "[%20s]\n", EMAIL );
+    printf( "[%-20s]\n", EMAIL );
+    printf( "[%.10s]\n", EMAIL );
+
+    float f = 12.123456789f;
+    printf( "%f\n", f );
+    printf( "%8f\n", f );
+    printf( "%.4f\n", f );
+    printf( "%8.4f\n", f );
+    printf( "%08.4f\n", f );
+    printf( "%-8.4f\n", f );
+    printf( "%e\n", f );
+    printf( "%g\n", f );
 }
