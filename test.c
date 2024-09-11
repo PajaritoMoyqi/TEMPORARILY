2931672ea1c75d4e57f848c81d83ba9b19fab078
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2024-09-11 21:52:33 +0900

    test.c for C Summary essay - structure

--- test.c
+++ test.c
@@ -1,16 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static int sv = 12345;
+#define MAX_NAME 10
 
-int show( void )
+struct person
 {
-    static int sv2 = 11111;
-    int lv = 11111;
-}
+    int age;
+    char* name;
+};
+
+struct person p1;
+struct person p2;
 
 int main( void )
-{
-    printf( "%d", sv );
+{   
+    // shallow copy
+    // p2.age = p1.age;
+    // p2.name = p1.name;
 
-    return 0;
+    // // deep copy
+    p1.name = (char*)malloc( sizeof( MAX_NAME + 1 ) );
+    p2.name = (char*)malloc( sizeof( MAX_NAME + 1 ) );
+    p1.age = 10;
+    p1.name = strcpy( p1.name, "John" );
+    // p1.name = "John";
+    p2.age = 10;
+    p2.name = strcpy( p1.name, "Home" );
+    // p2.name = "Home";
+    // // strcpy( p2.name, p1.name );
+    p2 = p1;
+
+    // // deep copy
+    // *p2.name = *p1.name;
+
+    p1.age = 20;
+    strcpy( p1.name, "JJJJ" );
+
+    printf( "%d, %s\n", p1.age, p1.name );
+    printf( "%d, %s\n", p2.age, p2.name );
 }
