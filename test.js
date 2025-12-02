64c2093a4fb7de3c30cbb49c02886438fa3af59b
Pajarito Moyqi <pajaritomoyqi@protonmail.ch>
2025-12-02 13:00:14 +0900

    test.js for new automation code

--- test.js
+++ test.js
@@ -1,9 +1,37 @@
-const slowExecution = async () => { 
-  console.log("Start"); 
-  await setTimeout(() => { 
-      console.log("This will run after 1 second"); 
-  }, 1000);
-  console.log("End"); 
+const startValue = '3.2';
+const Values = [
+  '1.677', '3.230', '3.444', '4.15', '9.999', '3', '3.2', '3.477', '3.333', '2.222'
+];
+Values.push( startValue );
+
+const findWidestGap = ( Arr ) => {
+  const length = Arr.length;
+  Arr.sort();
+
+  let low = -1, high = -1;
+  for ( let i = 0; i < length-1; i++ )
+  {
+    if ( Arr[i+1] - Arr[i] >= high - low )
+    {
+      if ( Arr[i+1] - Arr[i] == high - low )
+      {
+        if ( Math.random() <= 0.5 )
+        {
+          high = Arr[i+1];
+          low = Arr[i];
+        }
+      }
+      else
+      {
+        high = Arr[i+1];
+        low = Arr[i];
+      }
+    }
+  }
+
+  console.log( `%cBottom: ${low}`, 'color: #a4f644; background-color: #000;' );
+  console.log( `Top: ${high}` );
+  console.log( `My answer: ${(Number(low)+Number(high))/2}` );
 } 
 
-slowExecution(); 
+findWidestGap( Values );
